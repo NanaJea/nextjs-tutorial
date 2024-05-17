d7d751010076bf009be15c806de99e3be2996109
Jean Stéphane <ledroitalaporteedetous@gmail.com>
2024-05-17 10:19:08 +0200

    Aktualisierund Next.js Übung (Tutorial Full Stack Project).

---
+++ app/lib/mongodb.ts
@@ -0,0 +1,20 @@
+{/*import { MongoClient } from 'mongodb';
+
+const uri = 'votre_uri_de_connexion_mongodb';
+
+export async function connectToDatabase() {
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    try {
+        await client.connect();
+        console.log('Connecté à la base de données MongoDB');
+        return client.db();
+    } catch (error) {
+        console.error('Erreur de connexion à la base de données MongoDB', error);
+        throw error;
+    }
+}
+*/}
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -67,3 +67,6 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
     totalPages,
   ];
 };
+
+// Ajoutez le nouveau code ci-dessous
+// ...
